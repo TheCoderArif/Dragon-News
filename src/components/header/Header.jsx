248a71375277c61319e0dce276cd3f1dd4d804b7
TheCoderArif <mdarifulislam18539@gmail.com>
2026-02-27 09:41:01 +0600

    adding header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,17 @@
+import moment from 'moment';
+import logo from '../../assets/logo.png'
+const Header = () => {
+    return (
+        <div className='flex flex-col justify-center items-center'>
+            <img src={logo} alt="" className='' />
+            <h3 className='py-1 text-gray-500 text-xl'>Journalism Without Fear or Favour</h3>
+            <p>
+                {
+                    moment().format("dddd, MMMM Do YYYY, h:mm:ss a")
+                }
+            </p> 
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/layouts/home/Home.jsx
@@ -0,0 +1,15 @@
+import Header from "../../components/header/Header";
+
+const Home = () => {
+    return (
+        <div>
+            <header>
+                <Header></Header>
+            </header>
+            <nav></nav>
+            <footer></footer>
+        </div>
+    );
+};
+
+export default Home;
