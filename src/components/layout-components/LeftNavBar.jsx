9aa6c8ff83d733f0ec630326459ed35ee7ed791d
TheCoderArif <mdarifulislam18539@gmail.com>
2026-02-27 18:36:04 +0600

    adding left aside

---
+++ src/components/layout-components/LeftNavBar.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+const LeftNavBar = () => {
+    const [catagories, setCategories] = useState([]);
+    useEffect(() => {
+        fetch('https://openapi.programming-hero.com/api/news/categories')
+        .then(res => res.json())
+        .then(data => setCategories(data.data.news_category))
+    },[]);
+    return (
+        <div>
+            <h2 className="font-semibold mb-2">All Categories ({catagories.length})</h2>
+            <div className="flex flex-col">
+                {
+                    catagories.map(category => <button className="btn mx-5 mb-2" key={category.category_id}>{category.category_name}</button>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default LeftNavBar;
--- src/layouts/home/Home.jsx
+++ src/layouts/home/Home.jsx
@@ -1,30 +1,33 @@
 import Header from "../../components/header/Header";
 import LatestNews from "../../components/latest-news/LatestNews";
 import NavBar from "../../components/navBar/NavBar";
+import LeftNavBar from "../../components/layout-components/LeftNavBar";
 
 const Home = () => {
-    return (
-        <div className="font-poppins">
-            <header>
-                <Header></Header>
-                <section className="w-11/12 mx-auto py-1.5  my-2">
-                    <LatestNews></LatestNews>
-                </section>
-            </header>
+  return (
+    <div className="font-poppins">
+      <header>
+        <Header></Header>
+        <section className="w-11/12 mx-auto py-1.5  my-2">
+          <LatestNews></LatestNews>
+        </section>
+      </header>
 
-            <nav className="w-11/12 mx-auto">
-                <NavBar></NavBar>
-            </nav>
+      <nav className="w-11/12 mx-auto">
+        <NavBar></NavBar>
+      </nav>
 
-            <main className="py-6 w-11/12 mx-auto grid md:grid-cols-12 gap-3">
-                <aside className="left col-span-3"> left aside </aside>
-                <section className="main col-span-6">Main</section>
-                <aside className="right col-span-3">right</aside>
-            </main>
+      <main className="py-6 w-11/12 mx-auto grid md:grid-cols-12 gap-3">
+        <aside className="left col-span-3">
+          <LeftNavBar></LeftNavBar>
+        </aside>
+        <section className="main col-span-6">Main</section>
+        <aside className="right col-span-3">right</aside>
+      </main>
 
-            <footer></footer>
-        </div>
-    );
+      <footer></footer>
+    </div>
+  );
 };
 
 export default Home;
